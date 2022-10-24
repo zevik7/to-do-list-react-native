28c471f0147eed290ef0d0f62189aa82f10b0642
zevik7@gmail.com <zevik2807@gmail.com>
2022-10-24 23:19:38 +0700

    Inital commit

---
+++ src/Containers/HomeContainer.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import {
+  View,
+  ActivityIndicator,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native'
+import { useDispatch } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import { Brand } from '@/Components'
+import { useTheme } from '@/Hooks'
+import { useLazyFetchOneQuery } from '@/Services/modules/users'
+import { changeTheme, ThemeState } from '@/Store/Theme'
+
+const HomeContainer = () => {
+  const { t } = useTranslation()
+  const { Common, Fonts, Gutters, Layout } = useTheme()
+  const dispatch = useDispatch()
+
+  const [fetchOne, { data, isSuccess, isLoading, isFetching, error }] =
+    useLazyFetchOneQuery()
+
+  return (
+    <ScrollView
+      style={Layout.fill}
+      contentContainerStyle={[
+        Layout.fill,
+        Layout.colCenter,
+        Gutters.smallHPadding,
+      ]}
+    >
+      <View style={[[Layout.colCenter, Gutters.smallHPadding]]}>
+        <Brand />
+      </View>
+      <View
+        style={[
+          Layout.row,
+          Layout.rowHCenter,
+          Gutters.smallHPadding,
+          Gutters.largeVMargin,
+          Common.backgroundPrimary,
+        ]}
+      />
+    </ScrollView>
+  )
+}
+
+export default HomeContainer
---
+++ src/Navigators/Main.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { HomeContainer } from '@/Containers'
+
+const Tab = createBottomTabNavigator()
+
+// @refresh reset
+const MainNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeContainer}
+        options={{
+          tabBarIconStyle: { display: 'none' },
+          tabBarLabelPosition: 'beside-icon',
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
+
+export default MainNavigator
